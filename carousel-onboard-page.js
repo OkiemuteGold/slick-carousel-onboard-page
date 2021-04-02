013575aab5f9660f2b703101dce38ab85a420c74
Okiemute Gold <goldokiemute@gmail.com>
2021-04-02 20:51:23 +0100

    add slick carousel for sliding onboard message or image

--- carousel-onboard-page.js
+++ carousel-onboard-page.js
@@ -1,17 +1,33 @@
 $(document).ready(function () {
     // slick
-    // $(".onboard-summary-img").slick({
-    //     accessibility: true,
-    //     autoplay: true,
-    //     infinite: true,
-    //     slidesToShow: 1,
-    //     autoplaySpeed: 2000,
-    //     dots: true,
-    //     arrows: false,
-    //     draggable: true,
-    //     lazyLoad: 'ondemand',
-    //     centerMode: true
-    // });
+    $(".onboard-summary-img").slick({
+      accessibility: true,
+      autoplay: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: "20px",
+      autoplaySpeed: 3000,
+      dots: true,
+      arrows: false,
+      draggable: true,
+      // lazyLoad: "ondemand",
+      // responsive: [
+      //   {
+      //     breakpoint: 769,
+      //     settings: {
+      //       slidesToShow: 1,
+      //       slidesToScroll: 1,
+      //       centerMode: true,
+      //       centerPadding: "20px"
+      //     },
+      //   }
+      //   // You can unslick at a given breakpoint now by adding:
+      //   // settings: "unslick"
+      //   // instead of a settings object
+      // ],
+    });
     
     // footer year change
     let fullYear;
